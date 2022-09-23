9331c3a07e69c4b89b3eee804d7379ab76c71e83
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-09-23 21:04:53 +0330

    Brackets

---
+++ src/code/Brackets.js
@@ -0,0 +1,31 @@
+// A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+//
+// S is empty;
+// S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+// S has the form "VW" where V and W are properly nested strings.
+//     For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+
+module.exports = (S) => {
+    let buffer = [];
+    let opens = '([{';
+    let closes = ')]}';
+
+    for (const item of S) {
+        if (opens.indexOf(item) > -1) {
+            buffer.push(item);
+            continue;
+        }
+        if (!buffer.length) {
+            return 0;
+        }
+        if (closes.indexOf(item) !== opens.indexOf(buffer.pop())) {
+            return 0;
+        }
+    }
+
+    if (!buffer.length) {
+        return 1;
+    }
+
+    return 0;
+};
---
+++ test/code/Brackets.spec.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+const solution = require('../../src/code/Brackets');
+const assert = require('assert');
+test('should return correctly', function () {
+    assert.deepEqual(solution(''), 1);
+    assert.deepEqual(solution('{[()()]}'), 1);
+    assert.deepEqual(solution('()()'), 1);
+    assert.deepEqual(solution('()(){}'), 1);
+    assert.deepEqual(solution('{[()()]}()'), 1);
+    assert.deepEqual(solution('([)()]'), 0);
+});
